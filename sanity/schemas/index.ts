c2e8149b074aa404239c84575e0b44ea739e4406
Guilherme Illescas <gui.illescas@icloud.com>
2025-12-30 23:46:11 -0300

    chore: created sanity about schema

--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -2,6 +2,7 @@ import { post } from "./post"
 import { category } from "./category"
 import { author } from "./author"
 import { blockContent } from "./blockContent"
+import { about } from "./about"
 
-export const schemas = [post, category, author, blockContent]
+export const schemas = [post, category, author, blockContent, about]
 
